eb76e08805bdcc2465ba78716a99ce74a59a65df
CallMeJooj <j.henrique@edu.pucrs.br>
2024-09-17 09:31:41 -0300

    Added time count to main thread

--- chandy-misra/src/main.cpp
+++ chandy-misra/src/main.cpp
@@ -10,7 +10,7 @@
  * USEFUL REFERENCE:
  *    -> Pthreads: https://computing.llnl.gov/tutorials/pthreads/
  *    -> Dining Philosophers Problem: https://en.wikipedia.org/wiki/Dining_philosophers_problem
-**********************************/
+ **********************************/
 #include <stdio.h>
 #include <iostream>
 #include <pthread.h>
@@ -28,54 +28,60 @@
 
 pthread_mutex_t global_lock_log;
 
-struct thread_data{
+struct thread_data
+{
     pd::Philosopher *philosopher;
 };
 
 /** Parse the command line parameters */
 cxxopts::ParseResult
-parse(int argc, char *argv[]) {
-    try {
+parse(int argc, char *argv[])
+{
+    try
+    {
         cxxopts::Options options(argv[0], "A CLI to visualize Philosophers Dinning Problem");
         options
-                .positional_help("[optional args]")
-                .show_positional_help();
+            .positional_help("[optional args]")
+            .show_positional_help();
 
         options
-                .add_options()
-                        ("n, num", "number of philosophers", cxxopts::value<int>())
-                        ("help", "Print help")
+            .add_options()("n, num", "number of philosophers", cxxopts::value<int>())("help", "Print help")
 #ifdef CXXOPTS_USE_UNICODE
 #endif
-                ;
+            ;
 
         auto result = options.parse(argc, argv);
 
-        if (result.count("num") == 0) {
+        if (result.count("num") == 0)
+        {
             std::cout << options.help({""}) << std::endl;
             exit(0);
         }
 
-        if (result.count("help")) {
+        if (result.count("help"))
+        {
             std::cout << options.help({""}) << std::endl;
             exit(0);
         }
 
         return result;
-
-    } catch (const cxxopts::OptionException &e) {
+    }
+    catch (const cxxopts::OptionException &e)
+    {
         std::cout << "error parsing options: " << e.what() << std::endl;
         exit(1);
     }
 }
 
 /** Dine worker function */
-void* dine(void* threadarg) {
-    struct thread_data *local_data = (struct thread_data *) threadarg;
-    pd::Philosopher* philosopher = local_data->philosopher;
+void *dine(void *threadarg)
+{
+    struct thread_data *local_data = (struct thread_data *)threadarg;
+    pd::Philosopher *philosopher = local_data->philosopher;
     sleep(1);
 
-    while(philosopher->isSit) {
+    while (philosopher->isSit)
+    {
         philosopher->think();
         philosopher->eat();
     }
@@ -84,13 +90,14 @@ void* dine(void* threadarg) {
 }
 
 /** Main function */
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     /*** Parse the command line parameters */
-    
 
     auto result = parse(argc, argv);
     int num = 5;
-    if (result.count("num")) {
+    if (result.count("num"))
+    {
         num = result["num"].as<int>();
     }
     pthread_mutex_t topic_mutex;
@@ -102,26 +109,30 @@ int main(int argc, char *argv[]) {
     pthread_cond_init(&cv, NULL);
     pthread_mutex_init(&topic_mutex, NULL);
     pthread_mutex_init(&global_lock_log, NULL);
-    for(int i = 0; i < num; i++) {
+    for (int i = 0; i < num; i++)
+    {
         pthread_mutex_init(&mutex_forks[i], NULL);
     }
-    pd::Topic topic {
+    pd::Topic topic{
         cv,
-        topic_mutex
-    };
+        topic_mutex};
 
     /** Initialize philosophers and forks */
-    std::vector<pd::Philosopher*> philosophers;
-    std::vector<pd::Fork*> forks;
+    std::vector<pd::Philosopher *> philosophers;
+    std::vector<pd::Fork *> forks;
 
-    std::cout << "Creating "<< num << " philosophers........." << std::endl;
-    for(int i = 0; i < num; i++) {
+    std::cout << "Creating " << num << " philosophers........." << std::endl;
+    for (int i = 0; i < num; i++)
+    {
         forks.push_back(new pd::Fork(i + 1, i % num + 1, mutex_forks[i], topic));
     }
-    for(int i = 0; i < num; i++) {
+    for (int i = 0; i < num; i++)
+    {
         philosophers.push_back(new pd::Philosopher(i + 1, *forks[i], *forks[(i + 1) % num], global_lock_log));
     }
 
+    const std::chrono::time_point<std::chrono::steady_clock> start =
+        std::chrono::steady_clock::now();
     /** Create threads and start work */
     std::cout << "Start the Dinner!!" << std::endl;
     pthread_attr_t attr;
@@ -130,12 +141,14 @@ int main(int argc, char *argv[]) {
     struct thread_data thread_data_array[num];
 
     // create threads and start work
-    for(int tid = 0; tid < num; tid++) {
+    for (int tid = 0; tid < num; tid++)
+    {
         thread_data_array[tid].philosopher = philosophers[tid];
 
-        int rc = pthread_create(&threads[tid], &attr, dine, (void *) &thread_data_array[tid]);
+        int rc = pthread_create(&threads[tid], &attr, dine, (void *)&thread_data_array[tid]);
 
-        if (rc){
+        if (rc)
+        {
             printf("ERROR; return code from pthread_create() is %d\n", rc);
             exit(-1);
         }
@@ -145,12 +158,14 @@ int main(int argc, char *argv[]) {
     sleep(DINING_TIME);
 
     /** End the dining */
-    for(int i = 0; i < num; i++) {
+    for (int i = 0; i < num; i++)
+    {
         philosophers[i]->isSit = false;
     }
 
     // wait for all threads to complete their work
-    for(int tid = 0; tid < num; tid++) {
+    for (int tid = 0; tid < num; tid++)
+    {
         std::cout << "Philosopher " << tid << " is done dining." << std::endl;
         int rc = pthread_join(threads[tid], NULL);
         if (rc)
@@ -159,11 +174,14 @@ int main(int argc, char *argv[]) {
             exit(-1);
         }
     }
-
+    const auto end = std::chrono::steady_clock::now();
     std::cout << "All philosophers are done dining." << std::endl;
 
+    std::cout << "Time taken: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms" << std::endl;
+
     /** Release resources */
-    for(int i = 0; i < num; i++) {
+    for (int i = 0; i < num; i++)
+    {
         delete philosophers[i];
         delete forks[i];
     }
